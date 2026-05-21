8806657ecf6658b83fbf3d5e0b32434c1d2e7741
@SimranjeetKaur <simranjeet1136.be23@chitkara.edu.in>
2026-05-22 00:44:24 +0530

    use brevo smtp

--- server/src/utils/emailService.js
+++ server/src/utils/emailService.js
@@ -17,16 +17,13 @@ const createTransporter = () => {
     },
   });*/
 return nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT),
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 };
 
