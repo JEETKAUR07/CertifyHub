b79654ce0b39135aaafd1c475561b7f9d7b28131
@SimranjeetKaur <simranjeet1136.be23@chitkara.edu.in>
2026-05-21 15:53:32 +0530

    fix frontend routing

--- server/src/index.js
+++ server/src/index.js
@@ -94,7 +94,7 @@ const frontendPath = path.join(__dirname, "../../client/build");
 
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
