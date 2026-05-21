e4717f5a18f13febb1376ce8570d31622081b847
@SimranjeetKaur <simranjeet1136.be23@chitkara.edu.in>
2026-05-21 21:57:49 +0530

    fix gmail smtp

--- server/src/utils/emailService.js
+++ server/src/utils/emailService.js
@@ -17,11 +17,16 @@ const createTransporter = () => {
     },
   });*/
 return nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 };
 
