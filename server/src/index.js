a9dd53ae0a897bf21b4cbc679f7ae15913ccecca
@SimranjeetKaur <simranjeet1136.be23@chitkara.edu.in>
2026-05-22 01:35:43 +0530

    fix render binding

--- server/src/index.js
+++ server/src/index.js
@@ -23,9 +23,12 @@ const initServer = async () => {
   }
 
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
+  // app.listen(PORT, () => {
+  //   console.log(`🚀 Server running on port ${PORT}`);
+  // });
+  app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 };
 
 initServer();
